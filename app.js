0bd285f01a808863d4e5bab3140b230c809b4688
DevooKim <khwoo8026@gmail.com>
2021-01-12 03:29:07 +0900

    fix cors()

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: process.env.CORS_ADDRESS }));
+// app.use(cors());
 
 app.get("/test", (req, res, next) => {
   console.log("test");
