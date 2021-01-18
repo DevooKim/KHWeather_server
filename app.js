f2e49f1bdf12f4ece53f8eee2d99ca751d344903
DevooKim <khwoo8026@gmail.com>
2021-01-18 09:10:47 +0900

    .

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: process.env.CORS_ADDRESS }));
-// app.use(cors());
+// app.use(cors({ origin: process.env.CORS_ADDRESS }));
+app.use(cors());
 
 app.get("/test", (req, res, next) => {
   console.log("test");
